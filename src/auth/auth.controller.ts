7948f974fcae952f1f0bd9a3a37c21a3790c3dbe
anisul-islam-prog <kingfahad0802@gmail.com>
2022-08-08 09:22:23 +0600

    Prisma module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Post, Req } from "@nestjs/common";
-import { Request } from "express";
+import { Body, Controller, Post} from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
@@ -8,9 +8,9 @@ export class AuthController {
 
     }
     @Post('signup')
-    signup(@Body() dto: any) {
-        console.log({ dto, });
-        return this.authService.signup();
+    signup(@Body() dto: AuthDto) {
+        // console.log({ dto, });
+        return this.authService.signup(dto);
     }
     @Post('signin')
     signin() {
